77b446d480bf20d4fd0bd55cf281d40d9bde1e4c
Francesco Focher <francescofocher@gmail.com>
2026-01-18 16:50:25 +0100

    Add files via upload

--- src/inject/main.js
+++ src/inject/main.js
@@ -1,64 +1,96 @@
 (function() {
-    // 0. INIEZIONE STILI
-    if (window.aBetterPlace.Utils) {
-        window.aBetterPlace.Utils.injectStyles();
-    }
+    // --- CONFIGURAZIONE CENTRALIZZATA ---
+    let globalOptions = {
+        privacyMode: false,
+        toastNotifications: true,
+        customLogo: true,
+        checkUpdates: true
+    };
 
     // Funzione principale di update
     const performUpdates = () => {
         if (window.aBetterPlace.FormHandler) window.aBetterPlace.FormHandler.process();
-        if (window.aBetterPlace.DialogHandler) window.aBetterPlace.DialogHandler.process();
-        
+        if (window.aBetterPlace.DialogHandler) window.aBetterPlace.DialogHandler.process(globalOptions);
+                
         if (window.aBetterPlace.LayoutHandler) window.aBetterPlace.LayoutHandler.process();
-        if (window.aBetterPlace.LogoHandler) window.aBetterPlace.LogoHandler.process();
+        if (window.aBetterPlace.LogoHandler) window.aBetterPlace.LogoHandler.process(globalOptions);
         
         if (window.aBetterPlace.DateNav && !document.getElementById("better-nav-btns")) {
             window.aBetterPlace.DateNav.init();
         }
     };
 
-    if (window.aBetterPlace.Updater) {
-        setTimeout(() => window.aBetterPlace.Updater.check(), 2000);
-    }
-
-    const safeUpdate = window.aBetterPlace.Utils.debounce(performUpdates, 300);
-
-    // --- OBSERVER 1: BODY ---
-    const bodyObserver = new MutationObserver((mutations) => {
-        let shouldUpdate = false;
-        
-        for (const mutation of mutations) {
-            if (mutation.target.id === 'better-nav-btns' || mutation.target.id === 'better-toast') continue;
-
-            if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
-                shouldUpdate = true;
-                break; 
-            }
-            if (mutation.type === 'attributes') { 
-                shouldUpdate = true; 
-                break; 
-            }
+    const startSystem = () => {
+        // 0. INIEZIONE STILI
+        if (window.aBetterPlace.Utils) {
+            window.aBetterPlace.Utils.injectStyles();
         }
-        if (shouldUpdate) safeUpdate();
+
+        if (window.aBetterPlace.Updater) {
+            setTimeout(() => window.aBetterPlace.Updater.check(), 2000);
+        }
+
+        const safeUpdate = window.aBetterPlace.Utils.debounce(performUpdates, 300);
+
+        // --- OBSERVER 1: BODY ---
+        const bodyObserver = new MutationObserver((mutations) => {
+            let shouldUpdate = false;
+            
+            for (const mutation of mutations) {
+                if (mutation.target.id === 'better-nav-btns' || mutation.target.id === 'better-toast') continue;
+
+                if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+                    shouldUpdate = true;
+                    break; 
+                }
+                if (mutation.type === 'attributes') { 
+                    shouldUpdate = true; 
+                    break; 
+                }
+            }
+            if (shouldUpdate) safeUpdate();
+        });
+
+        bodyObserver.observe(document.body, {
+            childList: true,
+            subtree: true,
+            attributes: true,
+            attributeFilter: ["disabled", "class", "style"]
+        });
+
+        // --- OBSERVER 2: HTML ---
+        const htmlObserver = new MutationObserver((mutations) => {
+            if (window.aBetterPlace.LayoutHandler) window.aBetterPlace.LayoutHandler.process();
+        });
+
+        htmlObserver.observe(document.documentElement, {
+            attributes: true,
+            attributeFilter: ['style', 'class', 'overflow', 'padding-right']
+        });
+
+        // Primo avvio
+        performUpdates();
+    };
+
+    // --- INIT ---
+    // 1. Carichiamo le opzioni dallo storage
+    chrome.storage.sync.get(globalOptions, (items) => {
+        globalOptions = items;
+        startSystem();
     });
 
-    bodyObserver.observe(document.body, {
-        childList: true,
-        subtree: true,
-        attributes: true,
-        attributeFilter: ["disabled", "class", "style"]
+    // 2. Ascoltiamo cambiamenti futuri
+    chrome.storage.onChanged.addListener((changes, namespace) => {
+        if (namespace === 'sync') {
+            let changed = false;
+            for (let key in changes) {
+                if (globalOptions.hasOwnProperty(key)) {
+                    globalOptions[key] = changes[key].newValue;
+                    changed = true;
+                }
+            }
+            if (changed) performUpdates();
+        }
     });
-
-    // --- OBSERVER 2: HTML ---
-    const htmlObserver = new MutationObserver((mutations) => {
-        if (window.aBetterPlace.LayoutHandler) window.aBetterPlace.LayoutHandler.process();
-    });
-
-    htmlObserver.observe(document.documentElement, {
-        attributes: true,
-        attributeFilter: ['style', 'class', 'overflow', 'padding-right']
-    });
-
-    performUpdates();
 
 })();
